26a517a52c0b3cec4eedb6fe452fa6d62ada61aa
Orlin Dimitrov <orlin369@gmail.com>
2016-03-26 10:22:37 +0200

    Initial commit
    Initial commit.

---
+++ DiO_CS_BetaWorld/DiO_CS_BetaWorld/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiO_CS_BetaWorld
+{
+    /// <summary>
+    /// Main form application logic part.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        // -=== To do list ===-
+        // TODO: Install AForge image processing (Use PM console).
+        // Install-Package AForge.Imaging
+        // Install-Package AForge.Vision
+        // Install-Package AForge.Video
+        // TODO: Install AForge glyph recognizer (Refer to DiO_CS_GlyphRecognizer).
+        // TODO: Create frame graber (Form timer, no need for delegates to draw to the form.).
+        // TODO: Create video selector (FFMpeg). Use listing method from (DiO_CS_StereoScopic).
+        // TODO: Create video capture device (building capture device).
+        // TODO: Create image processor (Glyph Processor).
+        // TODO: Create robot controller (Betino). Will use standard (OR) protocol. The robot controller will be in different namespace.
+        // TODO: Use grid view for the responsive design.
+        // TODO: Use AppUtils helper class from (WCount or UViewr).
+        // 
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();            
+        }
+    }
+}
